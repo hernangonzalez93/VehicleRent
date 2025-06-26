afdb7e732cf45ae5f6b3be0a30d93051ad5e0f15
Hernan G <hernanc.9@hotmail.com>
2025-06-26 21:04:04 +0200

    more configuration on the repository

---
+++ src/microservice/GTMotive.microservice.Infrastructure/InfrastructureConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using GTMotive.microservice.ApplicationCore.Interfaces;
+using GTMotive.microservice.Infrastructure.MongoDb.Settings;
+using GTMotive.microservice.Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTMotive.microservice.Infrastructure
+{
+    /// <summary>
+    /// Configures and registers infrastructure services for the application.
+    /// </summary>
+    public static class InfrastructureConfiguration
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            // Register MongoDB settings from configuration
+            services.Configure<MongoDbSettings>(options => configuration.GetSection("MongoDb").Bind(options));
+
+
+            services.AddSingleton<IMongoClient>(sp =>
+            {
+                var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+                return new MongoClient(settings.ConnectionString);
+            });
+      
+
+            return services;
+        }
+    }
+}
--- src/microservice/GTMotive.microservice.Infrastructure/Repositories/VehicleRepository.cs
+++ src/microservice/GTMotive.microservice.Infrastructure/Repositories/VehicleRepository.cs
@@ -1,5 +1,6 @@
 ﻿using GTMotive.microservice.ApplicationCore.Interfaces;
 using GTMotive.microservice.Domain.Entities;
+using GTMotive.microservice.Infrastructure.MongoDb.Documents;
 using GTMotive.microservice.Infrastructure.MongoDb.Settings;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
@@ -11,31 +12,88 @@ using System.Threading.Tasks;
 
 namespace GTMotive.microservice.Infrastructure.Repositories
 {
+    /// <summary>
+    /// Provides methods for managing vehicle data in MongoDB database.
+    /// </summary>
+    /// <remarks>This repository is responsible for performing operations on vehicle data,</remarks>
     public class VehicleRepository : IVehicleRepository
     {
-        public Task AddAsync(Vehicle vehicle)
+        private readonly IMongoCollection<VehicleDocument> _collection;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VehicleRepository"/> class, providing access to the vehicle
+        /// collection in the MongoDB database.
+        /// </summary>
+        /// <remarks>This constructor sets up the repository by connecting to the specified MongoDB
+        /// database and initializing the vehicle collection.</remarks>
+        /// <param name="client">The <see cref="IMongoClient"/> used to connect to the MongoDB server.</param>
+        /// <param name="options">The configuration options containing the MongoDB database name.</param>
+        public VehicleRepository(IMongoClient client, IOptions<MongoDbSettings> options)
         {
-            throw new NotImplementedException();
+            var database = client.GetDatabase(options.Value.MongoDbDatabaseName);
+            _collection = database.GetCollection<VehicleDocument>("vehicles");
         }
 
-        public Task<Vehicle?> GetByIdAsync(string id)
+        /// <summary>
+        /// Asynchronously adds a new vehicle to the database.
+        /// </summary>
+        /// <param name="vehicle">The vehicle to add. Cannot be <see langword="null"/>.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        public async Task AddAsync(Vehicle vehicle)
         {
-            throw new NotImplementedException();
+            var doc = VehicleDocument.FromDomain(vehicle);
+            await _collection.InsertOneAsync(doc);
         }
 
-        public Task<bool> HasPersonRentedVehicleAsync(string personId)
+        /// <summary>
+        /// Retrieves a vehicle by its unique identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier of the vehicle to retrieve. Cannot be null or empty.</param>
+        /// <returns>A <see cref="Vehicle"/> object representing the vehicle with the specified identifier,  or <see
+        /// langword="null"/> if no matching vehicle is found.</returns>
+        public async Task<Vehicle?> GetByIdAsync(string id)
         {
-            throw new NotImplementedException();
+            var doc = await _collection.Find(v => v.Id == id).FirstOrDefaultAsync();
+            return doc?.ToDomain();
         }
 
-        public Task<List<Vehicle>> ListAsync()
+        /// <summary>
+        /// Retrieves a list of vehicles from the data source.
+        /// </summary>
+        /// <remarks>This method asynchronously fetches all vehicle records from the underlying collection
+        /// and converts them to domain objects. The returned list will be empty if no vehicles are found.</remarks>
+        /// <returns>A task representing the asynchronous operation. The task result contains a list of  <see cref="Vehicle"/>
+        /// objects representing the vehicles in the data source.</returns>
+        public async Task<List<Vehicle>> ListAsync()
         {
-            throw new NotImplementedException();
+            var docs = await _collection.Find(_ => true).ToListAsync();
+            return docs.Select(d => d.ToDomain()).ToList();
         }
 
-        public Task UpdateAsync(Vehicle vehicle)
+        /// <summary>
+        /// Updates the specified vehicle in the database asynchronously.
+        /// </summary>
+        /// <remarks>This method replaces the existing database record that matches the <see
+        /// cref="Vehicle.Id"/> of the provided <paramref name="vehicle"/>. Ensure that the <paramref name="vehicle"/>
+        /// object contains valid data before calling this method.</remarks>
+        /// <param name="vehicle">The vehicle to update. The <see cref="Vehicle.Id"/> property must match an existing record in the database.</param>
+        /// <returns>A task that represents the asynchronous update operation.</returns>
+        public async Task UpdateAsync(Vehicle vehicle)
         {
-            throw new NotImplementedException();
+            var doc = VehicleDocument.FromDomain(vehicle);
+            await _collection.ReplaceOneAsync(v => v.Id == vehicle.Id, doc);
+        }
+
+        /// <summary>
+        /// Determines whether the specified person has rented a vehicle.
+        /// </summary>
+        /// <remarks>This method queries the underlying data source to check if any vehicle is currently
+        /// rented by the specified person.</remarks>
+        /// <param name="personId">The unique identifier of the person to check. Cannot be null or empty.</param>
+        /// <returns><see langword="true"/> if the person has rented at least one vehicle; otherwise, <see langword="false"/>.</returns>
+        public async Task<bool> HasPersonRentedVehicleAsync(string personId)
+        {
+            return await _collection.Find(v => v.RentedBy == personId && v.IsRented).AnyAsync();
         }
     }
 }
